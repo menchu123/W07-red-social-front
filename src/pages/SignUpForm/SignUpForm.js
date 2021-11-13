ad8e75e2411636c45fe611ab85af96442f7885b6
Menchu MV <mcmvillalobos@gmail.com>
2021-11-13 22:05:35 +0100

    Added Sign Up page and routed App

---
+++ src/pages/SignUpForm/SignUpForm.js
@@ -0,0 +1,85 @@
+import "./SignUpForm.css";
+import { useEffect, useState } from "react";
+import useUser from "../../hooks/useUser";
+
+const SignUpForm = () => {
+  const { loginUser } = useUser();
+  const initialValues = {
+    username: "",
+    password: "",
+    name: "",
+  };
+
+  const [newUserData, setNewUserData] = useState(initialValues);
+  const [isDisabled, setIsDisabled] = useState(true);
+
+  useEffect(() => {
+    if (
+      newUserData.username !== "" &&
+      newUserData.password !== "" &&
+      newUserData.name !== ""
+    ) {
+      setIsDisabled(false);
+    } else {
+      setIsDisabled(true);
+    }
+  }, [newUserData.name, newUserData.password, newUserData.username]);
+
+  const onChange = (event) => {
+    setNewUserData({ ...newUserData, [event.target.id]: event.target.value });
+  };
+
+  const onLogin = (event) => {
+    event.preventDefault();
+    loginUser(newUserData);
+    setNewUserData(initialValues);
+  };
+
+  return (
+    <form
+      className="login-form"
+      noValidate
+      autoComplete="off"
+      onSubmit={onLogin}
+    >
+      <h5 className="login-title">Sign up</h5>
+      <div className="form-inputs">
+        <input
+          type="text"
+          className="input input--name"
+          id="name"
+          placeholder="Name"
+          value={newUserData.name}
+          onChange={(event) => onChange(event)}
+        />
+
+        <input
+          type="text"
+          className="input input--username"
+          id="username"
+          placeholder="Username"
+          value={newUserData.username}
+          onChange={(event) => onChange(event)}
+        />
+
+        <input
+          type="password"
+          className="input input--password"
+          id="password"
+          placeholder="Contraseña"
+          value={newUserData.password}
+          onChange={(event) => onChange(event)}
+        />
+
+        <button type="submit" className="login-btn" disabled={isDisabled}>
+          SIGN UP
+        </button>
+        <button className="sign-up-suggestion">
+          Already have an account? Log in!
+        </button>
+      </div>
+    </form>
+  );
+};
+
+export default SignUpForm;
