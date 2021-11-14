8ae45e6ac55e1dbc36a147c99dd2ba66396d41cb
Menchu MV <mcmvillalobos@gmail.com>
2021-11-14 19:53:53 +0100

    Sorted user list to hide logged user

--- src/pages/Homepage/Homepage.js
+++ src/pages/Homepage/Homepage.js
@@ -1,10 +1,16 @@
 import { useEffect } from "react";
 import User from "../../components/User/User";
+import useCurrentUser from "../../hooks/useCurrentUser";
 import useUserList from "../../hooks/useUserList";
 import "./Homepage.css";
 
 const Homepage = () => {
   const { loadUserList, userList } = useUserList();
+  const { loadCurrentUser, currentUser } = useCurrentUser();
+
+  useEffect(() => {
+    loadCurrentUser();
+  }, [loadCurrentUser]);
 
   useEffect(() => {
     loadUserList();
@@ -12,11 +18,13 @@ const Homepage = () => {
 
   return userList.length ? (
     <ul className="user-list">
-      {userList.map((user, index) => (
-        <li className="user-list-item" key={index}>
-          <User key={user.id} user={user} />
-        </li>
-      ))}
+      {userList
+        .filter((user) => user.username !== currentUser.username)
+        .map((user, index) => (
+          <li className="user-list-item" key={index}>
+            <User key={user.id} user={user} />
+          </li>
+        ))}
     </ul>
   ) : (
     <h2 className="loading">loading...</h2>
