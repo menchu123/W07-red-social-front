bd8ea7948d6e604255b2f2d19c51bbf6e39d55cd
Menchu MV <mcmvillalobos@gmail.com>
2021-11-14 18:34:20 +0100

    Created MyProfile page

--- src/pages/Homepage/Homepage.js
+++ src/pages/Homepage/Homepage.js
@@ -1,17 +1,15 @@
 import { useEffect } from "react";
 import User from "../../components/User/User";
-import useUser from "../../hooks/useUser";
 import useUserList from "../../hooks/useUserList";
 import "./Homepage.css";
 
 const Homepage = () => {
   const { loadUserList, userList } = useUserList();
-  const { loadCurrentUser } = useUser();
+
   useEffect(() => {
     loadUserList();
   }, [loadUserList]);
 
-  loadCurrentUser();
   return (
     <ul className="user-list">
       {userList.map((user, index) => (
